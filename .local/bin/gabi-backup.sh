f932eb72201226fb97eaa77bafc2e4ae3d892c3d
Gabriel Andrés Aguirre <gabiandiagui@gmail.com>
2022-12-15 12:24:16 -0300

    Comentarios a los scripts de sincronización

--- .local/bin/gabi-backup.sh
+++ .local/bin/gabi-backup.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+echo "Sincronizando Documentos"
 rsync -av --delete-after $HOME/Documentos/ gabi-sv:/mnt/datos/Gabi/Backups/Documentos
+
+echo "Sincronizando Imágenes"
 rsync -av --delete-after $HOME/Imágenes/ gabi-sv:/mnt/datos/Gabi/Backups/Imágenes
+
+echo "Sincronizando .gnupg"
 rsync -av --delete-after $HOME/.gnupg/ gabi-sv:/mnt/datos/Gabi/Backups/.gnupg
+
+echo "Sincronizando .password-store"
 rsync -av --delete-after $HOME/.password-store/ gabi-sv:/mnt/datos/Gabi/Backups/.password-store
+
+echo "Sincronizando .ssh"
 rsync -av --delete-after $HOME/.ssh/ gabi-sv:/mnt/datos/Gabi/Backups/.ssh
--- .local/bin/gabi-sync.sh
+++ .local/bin/gabi-sync.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+echo "Sincronizando Documentos"
 rsync -av --delete-after gabi-sv:/mnt/datos/Gabi/Backups/Documentos/ $HOME/Documentos
+
+echo "Sincronizando Imágenes"
 rsync -av --delete-after gabi-sv:/mnt/datos/Gabi/Backups/Imágenes/ $HOME/Imágenes
+
+echo "Sincronizando .gnupg"
 rsync -av --delete-after gabi-sv:/mnt/datos/Gabi/Backups/.gnupg/ $HOME/.gnupg
+
+echo "Sincronizando .password-store"
 rsync -av --delete-after gabi-sv:/mnt/datos/Gabi/Backups/.password-store/ $HOME/.password-store
+
+echo "Sincronizando .ssh"
 rsync -av --delete-after gabi-sv:/mnt/datos/Gabi/Backups/.ssh/ $HOME/.ssh
