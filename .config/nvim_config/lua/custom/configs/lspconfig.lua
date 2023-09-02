e1c0e6656b9a1c589e850615d3f174a723b9976d
Gabriel Andrés Aguirre <gabiandiagui@gmail.com>
2023-09-02 12:13:36 -0300

    Configuración de neovim para C/C++

--- .config/nvim_config/lua/custom/configs/lspconfig.lua
+++ .config/nvim_config/lua/custom/configs/lspconfig.lua
@@ -3,10 +3,18 @@ local configs = require("plugins.configs.lspconfig")
 local on_attach = configs.on_attach
 local capabilities = configs.capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 lspconfig.pyright.setup({
   on_attach = on_attach,
   capabilities = capabilities,
   filetypes = {"python"},
 })
+
+lspconfig.clangd.setup {
+  on_attach = function(client, bufnr)
+    client.server_capabilities.signatureHelpProvider = false
+    on_attach(client, bufnr)
+  end,
+  capabilities = capabilities,
+}
--- .config/nvim_config/lua/custom/plugins.lua
+++ .config/nvim_config/lua/custom/plugins.lua
@@ -1,5 +1,48 @@
 local plugins = {
   {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {}
+    },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, _)
+      require("core.utils").load_mappings("dap")
+    end
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
@@ -7,6 +50,7 @@ local plugins = {
         "lua",
         "python",
         "json",
+        "cpp",
       }
     },
   },
@@ -25,6 +69,9 @@ local plugins = {
         "mypy",
         "ruff",
         "pyright",
+        "clangd",
+        "clang-format",
+        "codelldb",
       },
     },
   },
